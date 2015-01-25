40fe282040cae4e189d03086651d8b6c3730e5eb
Tom Hutchinson <tomhut101@gmail.com>
2015-01-25 14:22:11 +0000

    Commit

---
+++ Fast/AppDelegate.m
@@ -0,0 +1,91 @@
+//
+//  AppDelegate.m
+//  Fast
+//
+//  Created by Tom Hutchinson on 18/01/2015.
+//  Copyright (c) 2015 Tom Hutchinson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "DetailViewController.h"
+#import "MasterViewController.h"
+#import "FTAPIClient.h"
+#import "FastKit.h"
+
+@interface AppDelegate () <UISplitViewControllerDelegate>
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
+    splitViewController.delegate = self;
+
+    UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
+    MasterViewController *controller = (MasterViewController *)masterNavigationController.topViewController;
+    controller.managedObjectContext = [TAHDataAccess sharedDataAccess].managedObjectContext;
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    [[TAHDataAccess sharedDataAccess] saveContext];
+}
+
+#pragma mark - Split view
+
+- (BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController {
+    if ([secondaryViewController isKindOfClass:[UINavigationController class]] && [[(UINavigationController *)secondaryViewController topViewController] isKindOfClass:[DetailViewController class]] && ([(DetailViewController *)[(UINavigationController *)secondaryViewController topViewController] detailItem] == nil)) {
+        // Return YES to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+//- (NSArray *)voucherSortDescriptors {
+//    NSSortDescriptor *sortDescriptor1 = [[NSSortDescriptor alloc] initWithKey:@"retailerID" ascending:YES];
+//    NSSortDescriptor *sortDescriptor2 = [[NSSortDescriptor alloc] initWithKey:@"voucherID" ascending:YES];
+//    return [[NSArray alloc] initWithObjects:sortDescriptor1, sortDescriptor2, nil];
+//}
+
+
+//- (void)application:(UIApplication *)application handleWatchKitExtensionRequest:(NSDictionary *)userInfo reply:(void (^)(NSDictionary *))reply{
+//    NSLog(@"LOL");
+//    if ([[userInfo objectForKey:@"request"] isEqualToString:@"Hello"]) {
+//        
+//        NSLog(@"containing app received message from watch");
+//        
+//        NSDictionary *response = @{@"response" : @"Watchkit"};
+//        reply(response);
+//    }
+//    reply(nil);
+//    
+//}
+
+@end
